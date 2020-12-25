e8130cd9c3bccec5f07fd65ebba1f3c56557d849
Jeff_Birt <birt_j@soigeneris.com>
2020-12-25 09:48:27 -0600

    Removed items that should not be synced

--- Software/Debug/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Jeff.000\AppData\Local\Temp\VMBuilds\M100_SRAM_Tester\mega_atmega2560\Debug\Adafruit_GFX_Library\glcdfont.c.o: \
- D:\Documents\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
--- Software/Debug/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Jeff.000\AppData\Local\Temp\VMBuilds\M100_SRAM_Tester\mega_atmega2560\Debug\Wire\Wire.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\Wire.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\utility/twi.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h
